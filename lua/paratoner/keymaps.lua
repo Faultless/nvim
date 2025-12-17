c7754e4f13be02a144423d0a7ea38b34a3ee4b62
Serge Kamel <s.kamel@uniconcreation.com>
2025-12-17 12:15:51 +0100

    replace old headers when detected.

--- lua/paratoner/keymaps.lua
+++ lua/paratoner/keymaps.lua
@@ -3,16 +3,34 @@
 -- Add any additional keymaps here
 
 local wk = require("which-key")
+
 wk.add({
-  { "<leader>h", group = "headers" }, -- group
+  { "<leader>h", group = "headers" },
   {
     "<leader>hc",
     mode = "n",
+    desc = "Insert or replace header comment",
     function()
-      local first_line = vim.api.nvim_buf_get_lines(0, 0, 1, false)[1]
-      if first_line and first_line:match("^/%*") then
-        return
+      local lines = vim.api.nvim_buf_get_lines(0, 0, 20, false) -- get top 20 lines
+      local start_idx, end_idx
+
+      -- Detect old header (/* ... */ block at the top)
+      for i, line in ipairs(lines) do
+        if i == 1 and line:match("^/%*") then
+          start_idx = 0
+        end
+        if start_idx and line:match("%*/") then
+          end_idx = i
+          break
+        end
       end
+
+      -- Remove old header if present
+      if start_idx and end_idx then
+        vim.api.nvim_buf_set_lines(0, start_idx, end_idx + 1, false, {})
+      end
+
+      -- Insert new header
       local header = {
         "/*",
         "* (C) Copyright Deli Home Holding B.V. 2025-2025 - All Rights Reserved",
@@ -23,9 +41,7 @@ wk.add({
         "*/",
         "",
       }
-
       vim.api.nvim_buf_set_lines(0, 0, 0, false, header)
     end,
-    desc = "Insert header comment",
   },
 })
